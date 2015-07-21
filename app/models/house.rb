66a944b6464fef6487afc6236a87a3ddeaa59b7c
Jacob Powers <powersjcb@gmail.com>
2015-07-20 17:55:30 -0700

    Make ActiveRecord 'listings' query with conditionals

--- app/models/house.rb
+++ app/models/house.rb
@@ -18,4 +18,43 @@
 class House < ActiveRecord::Base
   validates :street, :status, :price, :bedrooms,
     :bathrooms, :sq_ft, :lat, :lng, presence: true
+
+
+  # options: min_price, max_price, min_bed, max_bed, min_bath, max_bath
+  def listings(options)
+    queries = []
+    values  = []
+
+    if options[:min_price]
+      queries << "min_price >= ?"
+      values  << options[:min_price]
+    end
+    if options[:max_price]
+      queries << "min_price >= ?"
+      values  << options[:min_price]
+    end
+
+    if options[:min_bed]
+      queries << "min_bed >= ?"
+      values  << options[:min_bed]
+    end
+    if options[:max_bed]
+      queries << "max_bed >= ?"
+      values  << options[:max_bed]
+    end
+
+    if options[:min_bath]
+      queries << "min_bath >= ?"
+      values  << options[:min_bath]
+    end
+    if options[:max_bath]
+      queries << "max_bath >= ?"
+      values  << options[:max_bath]
+    end
+
+    queries.join(' AND ').concat(values)
+    
+    House.where(*args)
+  end
+
 end
